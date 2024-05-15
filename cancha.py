59c66591aeb6e18b612c846571023eb1d6173ec6
Enrique Ciriaco <enriqueciriaco.10@gmail.com>
2024-05-15 12:43:21 +0200

    actualizacion cancha

--- cancha.py
+++ cancha.py
@@ -5,7 +5,6 @@ class Cancha:
         self.precio = precio
         self.habilitada = habilitada
         self.reservas = []
-        self.empleados = []
 
     def __str__(self):
         return f"Cancha {self.numero} - Deporte: {self.deporte} - Precio: {self.precio} - Habilitada: {self.habilitada}"
@@ -13,13 +12,6 @@ class Cancha:
     def agregar_reserva(self, reserva):
         self.reservas.append(reserva)
 
-    def agregar_empleado(self, empleado):
-        self.empleados.append(empleado)
-
     def quitar_reserva(self, reserva):
         if reserva in self.reservas:
             self.reservas.remove(reserva)
-
-    def quitar_empleado(self, empleado):
-        if empleado in self.empleados:
-            self.empleados.remove(empleado)
